77640402b75eb96623a57076da8c19c3417364b8
buzz1675 <buzz1675@gmail.com>
2023-06-20 20:05:53 +0100

    make the two panels side by side

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import TrackList from '../TrackList/TrackList';
+import './SearchResults.css'
 
 const SearchResults = (props) => {
   return (
-    <div>
+    <div className='searchResults'>
       <h2>Search Results</h2>
       <TrackList tracks={props.tracks} onAdd={props.onAdd}/>
     </div>
